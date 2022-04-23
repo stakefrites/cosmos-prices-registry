be30d17c13955c3f8fb2449658e06542bc6b4c59
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-04-23 11:51:28 -0400

    get balance

--- utils/AprClient.mjs
+++ utils/AprClient.mjs
@@ -8,108 +8,162 @@ import {
   setupGovExtension,
 } from "@cosmjs/stargate";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+import { fromBech32 } from "@cosmjs/encoding";
 import CosmosDirectory from "./CosmosDirectory.mjs";
 
 const directory = CosmosDirectory();
 
-const getTokenInfo = (tokenData, data) => { 
-    const asset = tokenData.assets[0];
-    const base = asset.denom_units.find((el) => el.denom === asset.base);
-    const token = asset.denom_units.find((el) => el.denom === asset.display);
-    return {
-      denom: data.denom || base.denom,
-      symbol: data.symbol || token.denom,
-      decimals: data.decimals || token.exponent || 6,
-      image:
-        data.image ||
-        (asset.logo_URIs && (asset.logo_URIs.png || asset.logo_URIs.svg)),
-      coinGeckoId: asset.coingecko_id,
-    };
-}
+const getTokenInfo = (tokenData, data) => {
+  const asset = tokenData.assets[0];
+  const base = asset.denom_units.find((el) => el.denom === asset.base);
+  const token = asset.denom_units.find((el) => el.denom === asset.display);
+  return {
+    denom: data.denom || base.denom,
+    symbol: data.symbol || token.denom,
+    decimals: data.decimals || token.exponent || 6,
+    image:
+      data.image ||
+      (asset.logo_URIs && (asset.logo_URIs.png || asset.logo_URIs.svg)),
+    coinGeckoId: asset.coingecko_id,
+  };
+};
 
-class AprClient { 
-    constructor(chain) {
-        this.chain = chain;
-    }
-
-    async init() { 
-        const tokenData = await directory.getTokenData(this.chain.name)
-        this.tokenInfo = getTokenInfo(tokenData, this.chain)
-        this.rpcUrl = directory.rpcUrl(this.chain.name);
-        this.restUrl = directory.restUrl(this.chain.name);
-        const client = await this.makeClient();
-        this.client = client;
-        console.log(this)
-    }
-
-    async makeClient() {
-        const tmClient = await Tendermint34Client.connect(this.rpcUrl);
-        return CosmjsQueryClient.withExtensions(
-        tmClient,
-        setupStakingExtension,
-        setupBankExtension,
-        setupDistributionExtension,
-        setupMintExtension,
-        setupGovExtension
-        );
-    }
-
-    async getChainApr() {
-        const pool = await this.client.staking.pool();
-        const supply = await this.client.bank.supplyOf(this.tokenInfo.denom);
-        const bondedTokens = pool.pool.bondedTokens;
-        const totalSupply = supply.amount;
-        if (this.chain.chain_id.startsWith("osmosis")) {
-            const apr = await osmosisApr(totalSupply, bondedTokens);
-            return apr;
-            } else if (this.chain.chain_id.startsWith("sifchain")) {
-            const aprRequest = await axios.get(
-                "https://data.sifchain.finance/beta/validator/stakingRewards"
-            );
-            const apr = aprRequest.data.rate;
-            return apr;
-        } else {
-            const req = await this.client.mint.inflation();
-            const baseInflation = req.toFloatApproximation();
-            const ratio = bondedTokens / totalSupply;
-            const apr = baseInflation / ratio;
-            return apr;
-        }
+class AprClient {
+  constructor(chain) {
+    this.chain = chain;
   }
 
-    async osmosisApr(totalSupply, bondedTokens) {
-        const mintParams = await axios.get(
-        restUrl + "/osmosis/mint/v1beta1/params"
-        );
-        const osmosisEpochs = await axios.get(
-        restUrl + "/osmosis/epochs/v1beta1/epochs"
-        );
-        const epochProvisions = await axios.get(
-        restUrl + "/osmosis/mint/v1beta1/epoch_provisions"
-        );
-        const { params } = mintParams.data;
-        const { epochs } = osmosisEpochs.data;
-        const { epoch_provisions } = epochProvisions.data;
-        const mintingEpochProvision =
-        parseFloat(params.distribution_proportions.staking) * epoch_provisions;
-        const epochDuration = this.duration(epochs, params.epoch_identifier);
-        const yearMintingProvision =
-        (mintingEpochProvision * (365 * 24 * 3600)) / epochDuration;
-        const baseInflation = yearMintingProvision / totalSupply;
-        const bondedRatio = bondedTokens / totalSupply;
-        const apr = baseInflation / bondedRatio;
-        return apr;
-    }
+  async init() {
+    const tokenData = await directory.getTokenData(this.chain.name);
+    this.tokenInfo = getTokenInfo(tokenData, this.chain);
+    this.rpcUrl = directory.rpcUrl(this.chain.name);
+    this.restUrl = directory.restUrl(this.chain.name);
+    const client = await this.makeClient();
+    this.client = client;
+    console.log(this);
+  }
 
-    duration(epochs, epochIdentifier) {
-        const epoch = epochs.find((epoch) => epoch.identifier === epochIdentifier);
-        if (!epoch) {
+  async makeClient() {
+    const tmClient = await Tendermint34Client.connect(this.rpcUrl);
+    return CosmjsQueryClient.withExtensions(
+      tmClient,
+      setupStakingExtension,
+      setupBankExtension,
+      setupDistributionExtension,
+      setupMintExtension,
+      setupGovExtension
+    );
+  }
+
+  async getBalance(address) {
+    const liquid = await this.client.bank.allBalances(address);
+    console.log(liquid);
+    return liquid;
+  }
+
+  async getStaked(address) {
+    const stakingAcc = 0;
+    const staked = await this.client.staking.delegatorDelegations(address);
+    const totalTokensStaked =
+      staked.delegationResponses.length == 0
+        ? 0
+        : staked.delegationResponses.reduce((acc, item) => {
+            return acc + parseInt(item.balance.amount);
+          }, stakingAcc);
+    const stakedBalance = (
+      totalTokensStaked / Math.pow(10, this.tokenInfo.decimals)
+    ).toFixed(2);
+    return stakedBalance;
+  }
+
+  async getRewards(address) {
+    const rewardsAcc = 0;
+    const rewardsReducer = (acc, item) => {
+      if (item.reward.length == 0) {
         return 0;
-        }
+      } else {
+        return acc + parseInt(item.reward[0].amount);
+      }
+    };
+    const rewardsReq = await this.client.distribution.delegationTotalRewards(
+      address
+    );
+    const totalTokensRewards =
+      rewardsReq.rewards.length == 0
+        ? 0
+        : rewardsReq.rewards.reduce(rewardsReducer, rewardsAcc);
+    const rewards =
+      totalTokensRewards / Math.pow(10, this.tokenInfo.decimals + 18);
+    return rewards;
+  }
 
-        // Actually, the date type of golang protobuf is returned by the unit of seconds.
-        return parseInt(epoch.duration.replace("s", ""));
+  async getTotalBalances(address) {
+    const balances = await this.getBalance(address);
+    const rewards = await this.getRewards(address);
+    const staked = await this.getStaked(address);
+    return (
+      parseFloat(balances.find((el) => el.denom == this.tokenInfo.denom)) +
+      parseFloat(rewards) +
+      parseFloat(staked)
+    );
+  }
+
+  async getChainApr() {
+    const pool = await this.client.staking.pool();
+    const supply = await this.client.bank.supplyOf(this.tokenInfo.denom);
+    const bondedTokens = pool.pool.bondedTokens;
+    const totalSupply = supply.amount;
+    if (this.chain.chain_id.startsWith("osmosis")) {
+      const apr = await osmosisApr(totalSupply, bondedTokens);
+      return apr;
+    } else if (this.chain.chain_id.startsWith("sifchain")) {
+      const aprRequest = await axios.get(
+        "https://data.sifchain.finance/beta/validator/stakingRewards"
+      );
+      const apr = aprRequest.data.rate;
+      return apr;
+    } else {
+      const req = await this.client.mint.inflation();
+      const baseInflation = req.toFloatApproximation();
+      const ratio = bondedTokens / totalSupply;
+      const apr = baseInflation / ratio;
+      return apr;
     }
+  }
+
+  async osmosisApr(totalSupply, bondedTokens) {
+    const mintParams = await axios.get(
+      restUrl + "/osmosis/mint/v1beta1/params"
+    );
+    const osmosisEpochs = await axios.get(
+      restUrl + "/osmosis/epochs/v1beta1/epochs"
+    );
+    const epochProvisions = await axios.get(
+      restUrl + "/osmosis/mint/v1beta1/epoch_provisions"
+    );
+    const { params } = mintParams.data;
+    const { epochs } = osmosisEpochs.data;
+    const { epoch_provisions } = epochProvisions.data;
+    const mintingEpochProvision =
+      parseFloat(params.distribution_proportions.staking) * epoch_provisions;
+    const epochDuration = this.duration(epochs, params.epoch_identifier);
+    const yearMintingProvision =
+      (mintingEpochProvision * (365 * 24 * 3600)) / epochDuration;
+    const baseInflation = yearMintingProvision / totalSupply;
+    const bondedRatio = bondedTokens / totalSupply;
+    const apr = baseInflation / bondedRatio;
+    return apr;
+  }
+
+  duration(epochs, epochIdentifier) {
+    const epoch = epochs.find((epoch) => epoch.identifier === epochIdentifier);
+    if (!epoch) {
+      return 0;
+    }
+
+    // Actually, the date type of golang protobuf is returned by the unit of seconds.
+    return parseInt(epoch.duration.replace("s", ""));
+  }
 }
 
 export default AprClient;
